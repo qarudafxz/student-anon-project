d9279b15a9709a6177d549dfb7979156a335240a
qaruda <franfra10j@gmail.com>
2023-01-20 03:36:12 +0800

    student-anon commissioned projecct

---
+++ myreactapp/frontend/src/data/podcastUrls.js
@@ -0,0 +1,109 @@
+const URLS = [
+    {
+        "url": "https://open.spotify.com/embed/episode/35RtFXoB8d8ELFhc4WEI6i?utm_source=generator&t=0"
+    }, 
+    {
+        "url": "https://open.spotify.com/embed/episode/6I6fQjGvIhNRAwH3x8t7qp?utm_source=generator&t=0"
+    },
+    {
+        "url": "https://open.spotify.com/embed/episode/6JlLIpHOpxr55QkOanU1EJ?utm_source=generator&t=0"
+    },
+    {
+        "url": "https://open.spotify.com/embed/episode/0UdGB67FnRZ7WQkIChAc6q?utm_source=generator&t=0"
+    },
+    {
+        "url": "https://open.spotify.com/embed/episode/0YUMu8spulUxvNepOkvnby?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/7J7UM2fLnF9pm9fvANEDxA?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/1jRvvE6EKY0Kt535l1PXD1?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/6LPTBKSO2mluvoRy50bTbM?utm_source=generator&t=0"   
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/3zNEJ6jCK1JGPQVqEG2H36?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/4ThI9tUIP2yFglUetzd2DW?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/5f12e0plKEStUDSbafJWaf?utm_source=generator&t=0"   
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/4OHnMnaC83cULOkul327Cu?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/7rAm8K2cqANRq61joEAhl0?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/3XxY0ohJVn6q7jCTTISzO1?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/3VUja0uexmBvVfsAL4i9C7?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/6srpSsjI0knqKEp02HuyJp?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/2bMhIXfg7Ei9O7alDqOVKi?utm_source=generator&t=0"   
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/1nvfakqsPmdrRTMtKoRZ8E?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/5RzIH0pGUpAhb6r70OZBcY?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/4mAo9bdedkDKIQ7xUSFib4?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/17fkTAi9D044iWlt1oW1lx?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/16bEhwIgK22IUG7wuZMPRR?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/2Q7XxJTAz63rD7u0FuMQtu?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/06VDvo6IBZuZvazCGinhty?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/5IM3csHwSUhrMMXYJgUcB8?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/3MZN6iXWT4PwLJWZxaqZIe?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/31KuI5feJQQGfuvwlXKH5o?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/2HNI0BeF6WMI0DB02gKc8Q?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/4MD80YokLsr0Fa8YymYzEQ?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/48oIRU7lO0W5KapWEnZEqf?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/5vqmGFubmPtK9Rtel2mvVf?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/2kuo73rCLhSLT1YBlW1SEx?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/6cWIo2t6jUZyYSbs1cZswk?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/6zTjr32LLFzcVIx7jbH3rH?utm_source=generator&t=0"
+    },
+    {
+        "url" : "https://open.spotify.com/embed/episode/4nLPrBOYNXTiuhz0uMrYSf?utm_source=generator&t=0"
+    }
+];
+
+export default URLS;
--- myreactapp/frontend/src/index.js
+++ myreactapp/frontend/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import Messages from './pages/Messages';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import Messages from "./pages/Messages";
+import "./index.css";
 
 // const routerRoutes = createBrowserRouter([
 //     <Route path="/" element={<App />} />,
@@ -10,10 +11,10 @@ import './index.css';
 //     <Route path="/quotes" element={<AnonQuotes />} />,
 // ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    // <ErrorBoundary>
-        // <RouterProvider router={routerRoutes}/>
-    // {/* </ErrorBoundary> */}
-    <App />
-)
+	// <ErrorBoundary>
+	// <RouterProvider router={routerRoutes}/>
+	// {/* </ErrorBoundary> */}
+	<App />
+);
